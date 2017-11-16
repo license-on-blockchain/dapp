11e2d03418d4012a71b8fdf56998020ce23b52da
Alex Hoppen <alex@alexhoppen.de>
2017-11-16 18:12:38 +0100

    Add ability to view certificate text of an issuance

--- client/licenses.js
+++ client/licenses.js
@@ -24,3 +24,37 @@ Template.licenseRow.helpers({
         return Object.entries(this.balance.get()).reduce(([lhsAddress, lhsBalance], [rhsAddress, rhsBalance]) => lhsBalance.comparedTo(rhsBalance) < 0 ? [rhsAddress, rhsBalance] : [lhsAddress, lhsBalance])[0];
     }
 });
+
+Template.licenseRow.events({
+    'click a.showCertificate'() {
+        EthElements.Modal.show({
+            template: 'licenseCertificate',
+            data: {
+                licenseContract: Template.instance().data.licenseContract,
+                issuanceID: Template.instance().data.issuanceID,
+            },
+            class: 'certificateModal'
+        });
+    }
+});
+
+Template.licenseCertificate.onCreated(async function() {
+    // TODO: i18n
+    this.certificateText = new ReactiveVar("Loading…");
+    lob.getCertificateText(Template.instance().data.licenseContract, (error, value) => {
+        this.certificateText.set(value);
+    });
+});
+
+Template.licenseCertificate.helpers({
+    certificateText() {
+        // Split text on newlines to insert <br> for newlines in the template an retain safe substrings
+        return Template.instance().certificateText.get().split('\n');
+    }
+});
+
+Template.licenseCertificate.events({
+    'click button.hideModal'() {
+        EthElements.Modal.hide();
+    }
+});
